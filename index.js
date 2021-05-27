7943fd6207c59b4f3108e798c37b7357c480f1cb
Strongorange <74127841+Strongorange@users.noreply.github.com>
2021-05-27 20:27:53 +0900

    branch test

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 5000;
 
 const app = express();
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
 const handleHome = (req, res) => {
   return res.send("I love middlewares");
 };
 
-app.get("/", logger, handleHome);
+app.use(morgan("dev"));
+app.get("/", handleHome);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);

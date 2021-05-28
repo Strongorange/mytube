0c486aaebc73632115de5ee6491f0bd1977eb9d9
Strongorange <74127841+Strongorange@users.noreply.github.com>
2021-05-28 14:01:58 +0900

    5.10 Mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,32 @@
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "Video1",
+      rating: 5,
+      comments: 2,
+      createdAt: "1 mins ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Video2",
+      rating: 5,
+      comments: 5,
+      createdAt: "2 mins ago",
+      views: 13,
+      id: 2,
+    },
+    {
+      title: "Video3",
+      rating: 5,
+      comments: 2,
+      createdAt: "3 mins ago",
+      views: 9,
+      id: 3,
+    },
+  ];
+  res.render("home", { pageTitle: "Home", videos });
+};
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search Video");

e0d1b052bec2a6c34ff04dc4824c4f02439d34cb
Strongorange <74127841+Strongorange@users.noreply.github.com>
2021-06-03 10:12:29 +0900

    Day 9 FInish

--- src/movieController.js
+++ src/movieController.js
@@ -1,11 +1,13 @@
 import { getMovieById, getMovies, addMovie } from "./db";
 
+let newGenres = [];
+
 export const home = (req, res) =>
   res.render("movies", { movies: getMovies(), pageTitle: "Movies!" });
 
 export const movieDetail = (req, res) => {
   const {
-    params: { id }
+    params: { id },
   } = req;
   const movie = getMovieById(id);
   if (!movie) {
@@ -18,3 +20,27 @@ export const movieDetail = (req, res) => {
 Write the controller or controllers you need to render the form
 and to handle the submission
 */
+
+export const getUpload = (req, res) => {
+  res.render("upload", { pageTitle: "Add Movie!" });
+};
+
+export const postUpload = (req, res) => {
+  const newMovie = {
+    title: "",
+    synopsis: "",
+    genres: [],
+  };
+  newGenres = [];
+  const { title, synopsis, genres } = req.body;
+
+  newMovie.title = title;
+  newMovie.synopsis = synopsis;
+  newMovie.genres.push(genres);
+
+  console.log(newMovie);
+
+  addMovie(newMovie);
+
+  res.redirect("/");
+};
--- src/movieRouter.js
+++ src/movieRouter.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { home, movieDetail } from "./movieController";
+import { getUpload, home, movieDetail, postUpload } from "./movieController";
 
 const movieRouter = express.Router();
 
 // create the '/' route
+movieRouter.get("/", home);
 // create the /:id route
+movieRouter.get("/:id(\\d+)", movieDetail);
 // create the /add route (GET + POST)
+movieRouter.route("/add").get(getUpload).post(postUpload);
 
 export default movieRouter;

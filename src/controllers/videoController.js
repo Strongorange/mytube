ae83af55f0ff7777431eb963192ea8b36c48ca1a
Strongorange <74127841+Strongorange@users.noreply.github.com>
2021-06-08 22:17:06 +0900

    6.25

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -67,3 +67,11 @@ export const postUpload = async (req, res) => {
     });
   }
 };
+
+export const deleteVideo = async (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  await Video.findByIdAndDelete(id);
+  return res.redirect("/");
+};

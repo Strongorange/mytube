19b0463b72ee155fb5c89bd8392298b7f882943a
Strongorange <74127841+Strongorange@users.noreply.github.com>
2021-06-10 17:21:49 +0900

    7-4

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -10,7 +10,7 @@ export const postJoin = async (req, res) => {
   } = req;
 
   if (password !== password2) {
-    return res.render("join", {
+    return res.status(400).render("join", {
       pageTitle: "Join",
       errorMessage: "Password confirmation does not match",
     });
@@ -18,7 +18,7 @@ export const postJoin = async (req, res) => {
 
   const usernameExists = await User.exists({ $or: [{ username }, { email }] });
   if (usernameExists) {
-    return res.render("join", {
+    return res.status(400).render("join", {
       pageTitle: "Join",
       errorMessage: "This username/email already taken.",
     });

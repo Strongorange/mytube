001591bb9ad55e82c9d76be6fd1b469599eb777f
Strongorange <74127841+Strongorange@users.noreply.github.com>
2021-06-10 19:40:51 +0900

    7-9

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -69,6 +69,8 @@ export const postLogin = async (req, res) => {
     });
   }
   console.log("LOG USER IN COMING SOON!");
+  req.session.loggedIn = true;
+  req.session.user = user;
   return res.redirect("/");
 };
 

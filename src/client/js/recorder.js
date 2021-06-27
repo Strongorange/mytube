5c67fe1050a55862b5844ea415318d88eda7c7d6
Strongorange <74127841+Strongorange@users.noreply.github.com>
2021-06-27 23:15:21 +0900

    14-5 Thumbnail part 1

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,66 +1,89 @@
 import "regenerator-runtime/runtime.js";
 const { createFFmpeg, fetchFile } = require("@ffmpeg/ffmpeg");
 
-const startBtn = document.getElementById("startBtn");
+const actionBtn = document.getElementById("actionBtn");
 const video = document.getElementById("preview");
 
 let stream = null;
 let recorder = null;
 let videoFile = null;
 
+const files = {
+  input: "recording.webm",
+  output: "output.mp4",
+  thumb: "thumbnail.jpg",
+};
+
+const downloadFile = (fileUrl, fileName) => {
+  const a = document.createElement("a");
+  a.href = fileUrl;
+  a.download = fileName;
+  document.body.appendChild(a);
+  a.click();
+};
+
 const handleDownload = async () => {
+  actionBtn.removeEventListener("click", handleDownload);
+  actionBtn.innerHTML = "Transcoding....";
+  actionBtn.disabled = true;
+
   const ffmpeg = createFFmpeg({ log: true });
   await ffmpeg.load();
 
-  ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
-  await ffmpeg.run("-i", "recording.webm", "-r", "60", "recording.mp4");
+  ffmpeg.FS("writeFile", files.input, await fetchFile(videoFile));
+  await ffmpeg.run("-i", files.input, "-r", "60", files.output);
   await ffmpeg.run(
     "-i",
-    "recording.webm",
+    files.input,
     "-ss",
     "00:00:01",
     "-frames:v",
     "1",
-    "thumbnail.jpg"
+    files.thumb
   );
 
-  const mp4File = ffmpeg.FS("readFile", "recording.mp4");
-  const thumbFile = ffmpeg.FS("readFile", "thumbnail.jpg");
+  //FS.readfile 의 return 은 Uint8Array
+  const mp4File = ffmpeg.FS("readFile", files.output);
+  const thumbFile = ffmpeg.FS("readFile", files.thumb);
 
   console.log(mp4File);
   console.log(mp4File.buffer);
-
+  //Uint8Array 의 binary data 를 사용하기 위해서 buffer 사용
+  //JS 에서 파일같은 (file-like object) 객체를 만드려면 Blob 에 binary data 를 줘야함
   const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
   const thumbBlob = new Blob([thumbFile.buffer], { type: "image/jpg" });
 
   const mp4Url = URL.createObjectURL(mp4Blob);
   const thumbUrl = URL.createObjectURL(thumbBlob);
 
-  const a = document.createElement("a");
-  a.href = mp4Url;
-  a.download = "MyRecording.mp4";
-  document.body.appendChild(a);
-  a.click();
+  downloadFile(mp4Url, "recording.mp4");
+  downloadFile(thumbUrl, "thumbnail.jpg");
 
-  const thumbA = document.createElement("a");
-  thumbA.href = thumbUrl;
-  thumbA.download = "thumbnail.jpg";
-  document.body.appendChild(thumbA);
-  thumbA.click();
+  //속도를 위해 사용한 파일, URL 삭제
+  ffmpeg.FS("unlink", files.thumb);
+  ffmpeg.FS("unlink", files.input);
+  ffmpeg.FS("unlink", files.output);
+
+  URL.revokeObjectURL(thumbUrl);
+  URL.revokeObjectURL(mp4Url);
+  URL.revokeObjectURL(videoFile);
+
+  actionBtn.disabled = false;
+  actionBtn.innerHTML = "Record Again";
+  actionBtn.addEventListener("click", handleStart);
 };
 
 const handleStop = () => {
-  startBtn.innerHTML = "Download Recording";
-  startBtn.removeEventListener("click", handleStop);
-  startBtn.addEventListener("click", handleDownload);
-
+  actionBtn.innerHTML = "Download Recording";
+  actionBtn.removeEventListener("click", handleStop);
+  actionBtn.addEventListener("click", handleDownload);
   recorder.stop();
 };
 
 const handleStart = () => {
-  startBtn.innerHTML = "Stop Recording";
-  startBtn.removeEventListener("click", handleStart);
-  startBtn.addEventListener("click", handleStop);
+  actionBtn.innerHTML = "Stop Recording";
+  actionBtn.removeEventListener("click", handleStart);
+  actionBtn.addEventListener("click", handleStop);
 
   recorder = new MediaRecorder(stream);
   recorder.ondataavailable = (event) => {
@@ -84,4 +107,4 @@ const init = async () => {
 
 init();
 
-startBtn.addEventListener("click", handleStart);
+actionBtn.addEventListener("click", handleStart);
